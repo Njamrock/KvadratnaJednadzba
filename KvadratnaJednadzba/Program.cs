7c06955c00c660bc0c9db85e5af24a8031b6b4d2
Pavle Pavicic <PavleP@dspace.hr>
2021-10-21 19:57:32 +0200

    Zadatak2 completed.

--- KvadratnaJednadzba/Program.cs
+++ KvadratnaJednadzba/Program.cs
@@ -6,7 +6,18 @@ namespace KvadratnaJednadzba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QuadraticEquation qe = new QuadraticEquation(1, 2, -3);
+            var roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+            Console.WriteLine();
+
+            qe = new QuadraticEquation(-1, 2, -1);
+            roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
         }
     }
 }
--- KvadratnaJednadzba/QuadraticEquation.cs
+++ KvadratnaJednadzba/QuadraticEquation.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace KvadratnaJednadzba
 {
@@ -8,7 +6,7 @@ namespace KvadratnaJednadzba
     {
         public QuadraticEquation()
         {
-            
+
         }
 
         public QuadraticEquation(double a, double b, double c)
@@ -17,6 +15,46 @@ namespace KvadratnaJednadzba
             this.b = b;
             this.c = c;
         }
+
+        public double A
+        {
+            get { return a; }
+            set { this.a = value; }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set { this.b = value; }
+
+        }
+
+        public double C
+        {
+            get { return c; }
+            set { this.c = value; }
+
+        }
+
+        public double Y(double x)
+        {
+            return this.a * x * x + this.b * x + this.c;
+        }
+
+        public double Discriminant
+        {
+            get { return this.b * this.b - 4 * this.a * this.c; }
+        }
+
+        public double[] Roots
+        {
+            get
+            {
+                double sqrtDisc = Math.Sqrt(this.Discriminant);
+                return new double[] { (-this.b + sqrtDisc) / (2 * this.a), (-this.b - sqrtDisc) / (2 * this.a) };
+            }
+        }
+
         private double a;
         private double b;
         private double c;
