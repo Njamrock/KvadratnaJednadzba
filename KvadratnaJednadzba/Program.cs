8876ae237b34a68e8f052f0806a2065e40a566db
Pavle Pavicic <PavleP@dspace.hr>
2021-10-21 18:26:16 +0200

    Add project files.

---
+++ KvadratnaJednadzba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KvadratnaJednadzba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KvadratnaJednadzba/QuadraticEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvadratnaJednadzba
+{
+    class QuadraticEquation
+    {
+        public QuadraticEquation()
+        {
+            
+        }
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        private double a;
+        private double b;
+        private double c;
+    }
+}
