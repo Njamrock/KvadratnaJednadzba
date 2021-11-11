95e78c67063b15b89fd1e6e2a8e9a93da21056db
Pavle Pavicic <ppavicic@dspace.hr>
2021-11-11 18:48:08 +0100

    Added message logger.

--- Gui/Program.cs
+++ Gui/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Windows.Forms;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace VsiteCSharpKvadratnaJednadzbaGui
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+            (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            log.Info("Program is starting.");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            log.Info("Program is ending.");
         }
     }
 }
